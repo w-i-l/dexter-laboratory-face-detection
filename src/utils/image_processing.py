2412a7a454f2f8fbb557ab2b41777667090e734b
Mihai Ocnaru <mihai.ocnaru@mready.net>
2024-12-28 22:26:51 +0200

    added reading image and resize it

--- src/utils/image_processing.py
+++ src/utils/image_processing.py
@@ -4,6 +4,10 @@ import numpy as np
 import cv2
 
 class ImageProcessing:
+
+    IMAGE_WIDTH = 130
+    IMAGE_HEIGHT = 116
+
     @staticmethod
     def crop_image(image_annotation: ImageAnnotation) -> np.ndarray:
         image = cv2.imread(image_annotation.image_path)
@@ -14,3 +18,25 @@ class ImageProcessing:
         bounding_box = image_annotation.bounding_box
 
         return image[bounding_box.ymin:bounding_box.ymax, bounding_box.xmin:bounding_box.xmax]
+    
+    @staticmethod
+    def read_image(image_path: str) -> np.ndarray:
+        image = cv2.imread(image_path)
+
+        if image is None:
+            raise FileNotFoundError(f"File {image_path} not found")
+
+        width, height = ImageProcessing.IMAGE_WIDTH, ImageProcessing.IMAGE_HEIGHT
+        image = cv2.resize(image, (height, width))
+        # image = np.expand_dims(image, axis=1)
+        # image = np.transpose(image, (1, 0, 2))  # Transpose height and width dimensions
+        return image
+    
+
+    @staticmethod
+    def resize_image(image: np.ndarray) -> np.ndarray:
+        width, height = ImageProcessing.IMAGE_WIDTH, ImageProcessing.IMAGE_HEIGHT
+        image = cv2.resize(image, (height, width))  # cv2.resize takes (width, height)
+        # image = np.expand_dims(image, axis=1)
+        print("image shape", image.shape)
+        return image
